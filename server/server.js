56d75ed5f76e5ff9f4bc9c3b140b2efceb878dd3
Dalton Stevens <d.72.stevens@gmail.com>
2023-06-23 13:59:10 -0500

    added rollbar

--- server/server.js
+++ server/server.js
@@ -8,6 +8,14 @@ app.use(express.json());
 app.use(cors());
 app.use(express.static('public'))
 
+let Rollbar = require('rollbar')
+let rollbar = new Rollbar({
+  accessToken: 'a9251e35bf2d4b7cb5a0c0f785baff3d',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
+rollbar.log('Hello world!')
+
 // console.log(__dirname,'../public/index.html');
 
 app.get('/', (req, res) => {
